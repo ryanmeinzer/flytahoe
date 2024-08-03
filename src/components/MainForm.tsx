bd5649259744e2f6474849ab55c71a8d65901b6c
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2024-08-02 21:06:47 -0700

    adding back in opacity

---
+++ src/components/MainForm.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useState} from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+const MainForm = () => {
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/; // Simplified regex for email validation
+    return emailRegex.test(email);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const emailInput = e.target.value;
+    setEmail(emailInput);
+    if (!validateEmail(emailInput) && emailInput.length > 0) {
+      setEmailError('Please enter a valid email address.');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const isButtonDisabled = !email || !!emailError; // ensures result is always boolean
+
+  const handleRegister = async () => {
+    await fetch('/api/snowboard/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ serial_number: parseInt(serial), make, model, size: parseInt(size), email }),
+    });
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography variant="h4" gutterBottom>
+          Snowboard Registry
+        </Typography>
+        <TextField
+          fullWidth
+          label="Email"
+          type="email"
+          variant="outlined"
+          value={email}
+          onChange={handleEmailChange}
+          error={!!emailError}
+          helperText={emailError}
+          sx={{ mb: 2 }}
+        />
+        <Box>
+          <Grid
+            container
+            spacing={4}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Button variant="contained" onClick={handleRegister} disabled={isButtonDisabled}>
+                Register
+              </Button>
+            </Grid>
+            <Grid item>
+              <Typography>
+                or
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default MainForm;
